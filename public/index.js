dc9f42d7187d57b40d3738907d211829ef92ff32
ftanekha <farai.tanekha@gmail.com>
2024-01-01 20:51:22 +0000

    Add user input field

--- public/index.js
+++ public/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hi world');
+document.addEventListener('DOMContentLoaded', () => {
+    const create = document.querySelector('#create');
+    create.addEventListener('click', (ev) => {
+        alert('button clicked');
+    });
+    const del = document.querySelector('#delete');
+    del.addEventListener('click', (ev) => {
+        alert('delete button clicked');
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hi world')
+document.addEventListener(
+    'DOMContentLoaded',
+    ()=> {
+        const create = document.querySelector('#create')!
+        create.addEventListener(
+            'click',
+            (ev: Event)=> {
+                alert('button clicked')
+            }
+        )
+        const del = document.querySelector('#delete')!
+        del.addEventListener(
+            'click',
+            (ev: Event)=> {
+                alert('delete button clicked')
+            }
+        )
+    }
+)
